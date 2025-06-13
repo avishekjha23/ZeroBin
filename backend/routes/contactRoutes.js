9547402cf67fe49530483efc0a0787ac0b171720
AVISHEK JHA <74662020+avishekjha23@users.noreply.github.com>
2025-06-14 01:48:40 +0530

    Delete backend/routes/contactRoutes.js

--- backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example contact route
-router.post('/', (req, res) => {
-  res.send('Contact form submitted');
-});
-
-module.exports = router;
