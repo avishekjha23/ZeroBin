5759634042c143ab7a19993b6135622748aaefb9
AVISHEK JHA <74662020+avishekjha23@users.noreply.github.com>
2025-06-14 01:48:47 +0530

    Delete backend/routes/garbageRoutes.js

--- backend/routes/garbageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example garbage route
-router.get('/', (req, res) => {
-  res.send('Garbage information');
-});
-
-module.exports = router;
